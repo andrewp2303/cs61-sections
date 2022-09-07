5ffea9581f800c648c60afcfa653111e1747ad87
ANDREW PALACCI <andrewpalacci@college.harvard.edu>
2022-09-07 16:19:42 -0400

    add some comments

--- datareps1/map1.cc
+++ datareps1/map1.cc
@@ -5,11 +5,13 @@
 std::map<uintptr_t, const char*> m;
 
 int main() {
+    // note that {} signify a k, v pair
     m.insert({0, "Hello"});
     m.insert({2, "!"});
     m.insert({1, "world"});
 
     for (auto it = m.begin(); it != m.end(); ++it) {
+        // interesting: uses %zu to reference uintptr_t, works for all *_t
         fprintf(stderr, "key %zu, value %s\n", it->first, it->second);
     }
 }
